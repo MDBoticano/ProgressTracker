2d8bdb6da92acf85c0e894c6b5e8af0fb924a565
Marlo Daniel Boticano <m.boticano@gmail.com>
2016-10-25 21:27:29 -0700

    added server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-//comment
+var express = require('express');
+var app = express();
+
+var PORT = 3000;
+
+//app.use(express.logger());
+//app.use(express.compress());
+
+app.use(express.static(__dirname + '/static'));
+
+var port = process.env.PORT || PORT;
+app.listen(port,function(){
+  console.log('Listening on port %s',port);
+})
