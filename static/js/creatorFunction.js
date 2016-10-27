fc9571d0cb108ae95617bcf16b9e4e245adf62f6
Danny Dang <dnd003@ucsd.edu>
2016-10-27 14:33:59 -0700

    added a new timer drop down menu on tracker creator page

--- static/js/creatorFunction.js
+++ static/js/creatorFunction.js
@@ -1,3 +1,5 @@
+
+/*** Add text fields ***/
 $(document).ready(function() {
     var max_fields      = 10; //maximum input boxes allowed
     var wrapper         = $(".input_fields_wrap"); //Fields wrapper
@@ -16,3 +18,30 @@ $(document).ready(function() {
         e.preventDefault(); $(this).parent('div').remove(); x--;
     })
 });
+
+
+
+
+/*** Select hours ***/
+$(function(){
+    var $select = $(".0-12");
+    for (i=0;i<=12;i++){
+        $select.append($('<option></option>').val(i).html(i))
+    }
+});
+
+/*** Select minutes ***/
+$(function(){
+    var $select = $(".0-60");
+    for (i=0;i<60;i+=5){
+        $select.append($('<option></option>').val(i).html(i))
+    }
+});
+
+
+
+
+
+
+
+
