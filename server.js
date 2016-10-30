9ebad741f5f70d7c45963ad79e0bae9031b8aa7d
Marlo Daniel Boticano <m.boticano@gmail.com>
2016-10-30 14:23:28 -0700

    Took code from Lab 5 to start using data.json

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 var express = require('express');
-//var http = require('http');
-//var path = require('path');
-var handlebars = require('express4-handlebars');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars');
+
+var index = require('./routes/index');
 
 var app = express();
 
+
+app.get('/',index.view);
+
+
+
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, '/public')));
+
 var PORT = 3000;
-
-/* Note this currently doesn't work right now. Error says this isn't included   
- * automatically in express, so if we want to use this, we gotta get it;
- * I tried npm install compress & npm install express-logger but it didn't work
- */
-//app.use(express.logger());
-//app.use(express.compress());
-
-app.use(express.static(__dirname + '/public'));
-
 var port = process.env.PORT || PORT;
+
 app.listen(port,function(){
   console.log('Server running on port %s',port);
 });
