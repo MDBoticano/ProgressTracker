8b4041629d9d873707ad20704833a9212ae058fb
MDBoticano <m.boticano@gmail.com>
2016-11-03 15:59:25 -0700

    removed current tracker creator html and js

--- public/js/progressTracker.js
+++ public/js/progressTracker.js
@@ -1,28 +1,6 @@
  $(document).ready(function(){  
      $(".button-collapse").sideNav(); //materialize navbar
       
-     //checkpoint add
-     var i=1;  
-      $('#add').click(function(){  
-           i++;  
-           $('#dynamic_field').append('<tr id="row'+i+'"><td><input type="text" name="name[]" placeholder="Enter checkpoint" class="form-control checkpoint_list" /></td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');  
-      });  
-      $(document).on('click', '.btn_remove', function(){  
-           var button_id = $(this).attr("id");   
-           $('#row'+button_id+'').remove();  
-      });  
-      $('#submit').click(function(){            
-           $.ajax({  
-                url:"name.php",  
-                method:"POST",  
-                data:$('#add_checkpoint').serialize(),  
-                success:function(data)  
-                {  
-                 alert(data);  
-                 $('#add_checkpoint')[0].reset();  
-                }  
-           });  
-      });  
  });  
 
 //Add friend button
