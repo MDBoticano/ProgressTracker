b3a0d6c548a3f4a44831579014f74025df525eb7
Marlo Daniel Boticano <m.boticano@gmail.com>
2016-11-03 23:09:50 -0700

    Edited tracker creator to have some more functionality

--- public/js/progressTracker.js
+++ public/js/progressTracker.js
@@ -1,44 +1,49 @@
- $(document).ready(function(){  
-     $(".button-collapse").sideNav(); //materialize navbar
+$(document).ready(function(){  
+    $(".button-collapse").sideNav(); //materialize navbar
       
-        var max_cp = 4;
-        var i = 1;
-        var x = i-1;
-        var selector = "#item_"+ i;
-        var done = "#item_"+ x;   
+    //Highlight Active collection
+    var max_cp = 4; //should count # of objects in the collection with checkpoint class
+    var i = 1;
+    var x = i-1;
+    var selector = "#item_"+ i;
+    var done = "#item_"+ x;   
      
      $("#CPstart").click(function(){
-        
-
+       console.log(x , i+1);
+       //Changes "Start" text to "Checkpoint"
+       if(i == 1){
+         $(this).html("Checkpoint");
+       }
+       
+       //Change the text when apporaching the final checkpoint
+       if(i == max_cp){
+         $(this).html("Finish");
+       }
+       
+       //Change the text to hint at home
+       if(i == max_cp+1){
+        $(this).html("Home");
+       }
+       
+       //Set the link so it goes home
+       if(i == max_cp+2){
+        $(this).attr("href", "/");
+       }
+       
+       
+       if(i <= max_cp+1){
         console.log("Clicked!"); 
-        //var selector = "#item_"+i;
-      
-        //while(i <= max_cp){     
-         //remove class CPactive on current, and add CPdone
-            
-        //$(done).removeClass("CPactive");
-            //$(done).addClass("CPdone");
-            $(selector).addClass("CPactive");
-            $(done).toggleClass("CPactive").addClass("CPdone");
-            console.log(selector);
-            //console.log("add success");
-            selector = "#item_"+ i;
-            done = "#item_"+ x;
-            //done = "#item_"+x;
+        $(selector).addClass("CPactive");
+        $(done).toggleClass("CPactive").addClass("CPdone");
+        console.log(selector);
 
-            x++;
-            i++;
-            //$(selector + 1).addClass("CPactive");
-        //$("#item_1").css("background-color", "yellow"); 
-        //console.log("yellow!");
-            //$(selector).removeClass("CPactive");
-        
-         
-
-         console.log("remove success");
-         //Add class CPactive to next on list,
-         //}
-
+        i++;
+        x++;
+        selector = "#item_"+ i;
+        done = "#item_"+ x;
+        console.log("remove success");
+       }
+       
      });
  });  
 
