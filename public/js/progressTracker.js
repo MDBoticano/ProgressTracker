2a6a01e2705054468b32ea197a133b0b3319ce01
Marlo Daniel Boticano <m.boticano@gmail.com>
2016-11-06 13:08:08 -0800

    Cleaned up progressTracker.js a bit, seems functional locally

--- public/js/progressTracker.js
+++ public/js/progressTracker.js
@@ -1,70 +1,18 @@
+//Global variables
+
+// -- for tracker creator checkpoint highlighting ------------------------
+var max_cp = $(".collection-item").length;
+var i = 0;
+var x = i-1;
+var selector = "#item_"+ i;
+var done = "#item_"+ x;   
+var timeTotal = 0;
+
 $(document).ready(function(){  
   
     initializePage();
     //CLEAN THIS UP, PUT THESE FUNCTIONS INTO THEIR OWN ONES (initializePage)
     //!!!PLEASE READ ABOVE!!!//
-  
-    
-      
-    //Highlight Active collection
-    var max_cp = $(".collection-item").length; //should count # of objects in the collection with checkpoint class
-    console.log(max_cp);
-    var i = 0;
-    var x = i-1;
-    var selector = "#item_"+ i;
-    var done = "#item_"+ x;   
-     
-  
-    var timeTotal = 0;
-     $("#CPstart").click(function(){
-       //console.log(x , i+1);
-       //Changes "Start" text to "Checkpoint"
-       if(i == 1){
-         $(this).html("Checkpoint");
-       }
-       
-       //Change the text when apporaching the final checkpoint
-       if(i == max_cp-1){
-         $(this).html("Finish");
-       }
-       
-       //Change the text to hint at home
-       if(i == max_cp){
-        var timeToComplete = (secondsTimeSpanToHMS(timeTotal));
-        var timeTotalString = '<center><li class="collection-item" id ="item_total">'+timeToComplete+'</li></center>';
-        $("#checkpoints").append(timeTotalString) 
-        $(this).html("Home");
-       }
-       
-       //Set the link so it goes home; change this to link to statistics or profile page or whatever else
-       if(i == max_cp+1){
-        $(this).attr("href", "/"); 
-       }
-       
-       if(i > 0 && 1 <= max_cp+1) {
-         var randTime = Math.floor((Math.random()*90)+90);
-         var timeToComplete = (secondsTimeSpanToHMS(randTime));
-         var TTCstring = " - " + timeToComplete;
-       
-       timeTotal += randTime;
-       //console.log(secondsTimeSpanToHMS(timeTotal));
-       }
-              
-       if(i <= max_cp+1){
-        console.log("Clicked!"); 
-        $(selector).addClass("CPactive");
-        $(done).toggleClass("CPactive").addClass("CPdone");
-        $(done).append(TTCstring); 
-        console.log(selector);
-
-        i++;
-        x++;
-        selector = "#item_"+ i;
-        done = "#item_"+ x;
-        console.log("remove success");
-       }
-       
-     });
  });  
 
 
@@ -72,9 +20,53 @@ $(document).ready(function(){
 function initializePage() {
   $(".button-collapse").sideNav(); //materialize navbar
   
+  $("#CPstart").click(highlightCP);
 }
 
-//
+// In tracker creator, highlights checkpoint based on button click
+function highlightCP(){
+     //console.log(x , i+1);
+     //Changes "Start" text to "Checkpoint"
+     if(i == 1){
+       $(this).html("Checkpoint");
+     }
+     //Change the text when apporaching the final checkpoint
+     if(i == max_cp-1){
+       $(this).html("Finish");
+     }
+     //Change the text to hint at home
+     if(i == max_cp){
+      var timeToComplete = (secondsTimeSpanToHMS(timeTotal));
+      var timeTotalString = '<center><li class="collection-item" id ="item_total">'+timeToComplete+'</li></center>';
+      $("#checkpoints").append(timeTotalString) 
+      $(this).html("Home");
+     }
+     //Set the link so it goes home; change this to link to statistics or profile page etc.
+     if(i == max_cp+1){
+      $(this).attr("href", "/"); 
+     }
+     //random time per task
+     if(i > 0 && i <= max_cp+1) {
+       var randTime = Math.floor((Math.random()*90)+90);
+       var timeToComplete = (secondsTimeSpanToHMS(randTime));
+       var TTCstring = " - " + timeToComplete;
+       timeTotal += randTime;
+     //console.log(secondsTimeSpanToHMS(timeTotal));
+     }     
+     if(i <= max_cp+1){
+      console.log("Clicked!"); 
+      $(selector).addClass("CPactive");
+      $(done).toggleClass("CPactive").addClass("CPdone");
+      $(done).append(TTCstring); 
+      console.log(selector);
+      i++;
+      x++;
+      selector = "#item_"+ i;
+      done = "#item_"+ x;
+      console.log("remove success");
+     }
+       
+}
 
 
 //Add friend button
