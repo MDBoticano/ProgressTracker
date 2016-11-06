50f2c4dbf8e0b03f88bca56c80e28c55ac59c5c9
Marlo Daniel Boticano <m.boticano@gmail.com>
2016-11-05 22:53:31 -0700

    Semi-functional, index for ids are off due to zero-indexing

--- routes/trackerCreator.js
+++ routes/trackerCreator.js
@@ -12,13 +12,13 @@ exports.generate = function(req, res){
   //console.log(taskID);
   //console.log(data); //displays data to console
   var navLinks = data.navLinks;
-  console.log(navLinks);
+  //console.log(navLinks);
   var location = data.location;
-  console.log(location);
+  //console.log(location);
   task = data.featuredTasks[taskID-1]; //Grabs the data in data array
-  console.log(task);
+  //console.log(task);
   
   var newData = {location, navLinks, task};
-  console.log(newData);
+  //console.log(newData);
   res.render('trackerCreator', newData);
 };
