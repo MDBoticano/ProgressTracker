2b41a314fd627a5ca2013ab53b1db6cad1d88b4e
MDBoticano <m.boticano@gmail.com>
2016-11-08 14:58:32 -0800

    Tagged pages are now properly populated, but data.json needs to be edited to determine what to populate it with

--- routes/tag.js
+++ routes/tag.js
@@ -6,15 +6,31 @@ exports.view = function(req, res){
 	res.render('tag',data);
 };
 
-//When url is "/tag/:id"
+//When url is "/tag/:tag"
 exports.generate = function(req, res){
   var tagURL = req.params.tag;
   tagURL = tagURL.toString();
   console.log(tagURL);
+  var tagID = data.tagURLtoID[tagURL];
+  //tagID = parseInt(tagID);
+  console.log(tagID);
   var navLinks = data.navLinks;
   var location = data.location;
-  var tagged; //searches data for all instances of "tagURL"
+  var tagged = []; 
   var newData = {location, navLinks, tagged};
+    console.log(newData);
+  for (i = 0; i < tagID.length; i++) {
+      var index =tagID[i];
+      var curData = data.featuredTasks[index-1];
+      //console.log(curData);
+      tagged.push(curData);
+  }
+  
+  //tagged = data.featuredTasks[tagID-1];
+  //console.log(tagged);
+    
+  
+  console.log(newData);
   
   res.render('tag', newData);
 };
