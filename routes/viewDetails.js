fd1afb8d24231a9e676263fba1d4da50a22b9cc9
Danny Dang <dnd003@ucsd.edu>
2016-11-10 15:20:12 -0800

    changes to tracker creator button

--- routes/viewDetails.js
+++ routes/viewDetails.js
@@ -10,6 +10,22 @@ exports.view = function(req, res){
 exports.generate = function(req, res){
 
   var urlString = req.params.id;
+  //console.log(typeof urlString);
+  urlString = urlString.toString(); //string version of url
+  //console.log(urlString);
+  var taskID = data.taskURLtoID[urlString]; //retrieve id if there is one
+  //console.log(taskID); //id recieved
+  //console.log(data); //displays data to console
+  var navLinks = data.navLinks;
+  //console.log(navLinks);
+  var location = data.location;
+  //console.log(location);
+  
+  task = data.featuredTasks[taskID-1]; //Grabs the data in data array, 0 index
+  //console.log(task);
+  
+  var newData = {location, navLinks, task};
+  //console.log(newData);
 
-  res.render('viewDetails', data);
+  res.render('viewDetails', newData);
 };
