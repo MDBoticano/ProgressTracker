f937f6edfd9940aca5204cedd3c5131426b06d5d
MDBoticano <m.boticano@gmail.com>
2016-11-10 17:30:45 -0800

    Show only featured tasks one per category on home page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,34 @@
 var data = require('../data.json');
 
-exports.view = function(req, res){
+exports.views = function(req, res){
 	//console.log(data); //displays data to console
 	res.render('index',data);
 };
+
+exports.view = function(req, res){
+    
+    var navLinks = data.navLinks;
+    var location = data.location;
+    //For each tag, grab the first value and add it 
+    var featuredTasks = [];
+    var numTagged = Object.keys(data.tagURLtoID).length; //Get number of tags
+    //console.log(numTagged);
+    
+    //Get the first value from each tag and put it into a new array
+    var firstTasks = [1,2,5,6,7,8]; //hardcoded 
+    //Remove any duplicates in the array
+    
+    //For each value in the array, get that featuredtask and add it to "featuredTasks"
+    for (i =-0; i < firstTasks.length; i++){
+        var index = firstTasks[i];
+        var curData = data.featuredTasks[index-1];
+        featuredTasks.push(curData);
+    }
+    
+    
+    var newData = {location, navLinks, featuredTasks};
+    
+    //console.log(newData);
+    
+    res.render('index',newData);
+}
