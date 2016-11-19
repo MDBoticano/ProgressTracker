fe48dd7dfbd69793df43af0a797cd1ae81d083f0
Marlo Daniel Boticano <m.boticano@gmail.com>
2016-11-19 01:43:04 -0800

    A/B testing code

--- public/js/progressTracker.js
+++ public/js/progressTracker.js
@@ -46,16 +46,14 @@ function initializePage() {
   
   //A/B Testing Events
   $("#origEdit").click(function (e){
-  e.preventDefault();
   console.log("clicked");
   ga('send','event','A-FAB','click');
-  }
+  });
                        
   $("#altEditor").click(function (e){
-    e.preventDefault();
     console.log("clicked");
     ga('send','event','B-Card','click');
-  }
+  });
       
 }
 
